eaeb953d28bb900b49098b7a46aae0500188e5bd
JamesThomasEdwards <digitalmediapro7@gmail.com>
2018-03-26 16:35:10 -0700

    added client containers and server route

--- src/server/server.js
+++ src/server/server.js
@@ -13,13 +13,15 @@ app.use(bodyParser.json());
 
 //  MONGOOSE
 mongoose.connection.once('open', () => {
-    console.log('===CONNECTED TO DATABASE===');
-  });
+  console.log('===CONNECTED TO DATABASE===');
+});
 
-  // COOKIES
+// COOKIES
 app.use(cookieParser('process.env.COOKIE_SECRET'));
 // 
-
+app.get('/', (req, res, next) => {
+  res.sendFile(path.join(__dirname, '../../index.html'))
+})
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => console.log('******SERVER LISTENING ON PORT 3000******'));
