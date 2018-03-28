3c06937eedab1951defc709b9bd987a74ea369f4
Monica <mshim89@gmail.com>
2018-03-27 21:51:09 -0700

    filtering for categories for api call

--- src/client/components/Landing/landingPageCategoryButtons.js
+++ src/client/components/Landing/landingPageCategoryButtons.js
@@ -3,10 +3,12 @@ import { Component } from 'react';
 import axios from 'axios';
 import RaisedButton from 'material-ui/RaisedButton';
 import '../../css/Landing.css';
+import CategoryList from '../Category/CategoryList'
+import { Link } from 'react-router-dom';
 
 const style = {
 
-    algorithms: {
+    algorithm: {
         margin: 12,
         width: 300,
         height: 50,
@@ -53,7 +55,8 @@ class LandingPageCategoryButtons extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            category: ''
+            category: '',
+            results: ''
         };
         this.handleClick = this.handleClick.bind(this);
     }
@@ -66,25 +69,35 @@ class LandingPageCategoryButtons extends Component {
             data: { category }
         })
         .then(res => {
-            const category = res.data;
-            // this.setState({ category });
-            console.log('this is the data sent', category); //check back here, why di dthis not console
-          })
+            this.setState({ 
+                category: category,
+                results: res.data.results
+            });
+            console.log('this is the data sent back from post', res.data.results); //check back here, why di dthis not console
+        })
     }
     // componentDidMount() {
     //     /////////
     // }
     render() {
+        const { category, results } = this.state;
         return (
+            <div>
             <div id="categoryContainer">
-            <RaisedButton label="Algorithms" value="Algorithms" primary={true} style={style.algorithms} onClick={this.handleClick} />
-            <br />
-            <RaisedButton label="System Design" value="System Design" primary={true} style={style.systemDesign} onClick={this.handleClick} />
-            <br />
-            <RaisedButton label="Front End" value="Front End" primary={true} style={style.frontEnd} onClick={this.handleClick} />
-            <br />
-            <RaisedButton label="Back End" value="Back End" primary={true} style={style.backEnd} onClick={this.handleClick} />
-        </div> 
+            <Link to = "/category" params= {{category: "category"}}>
+                <RaisedButton label="Algorithm" value="Algorithm" primary={true} style={style.algorithm} onClick={this.handleClick} />
+                </Link>
+                <br />
+                <RaisedButton label="System Design" value="System Design" primary={true} style={style.systemDesign} onClick={this.handleClick} />
+                <br />
+                <RaisedButton label="Front End" value="Front End" primary={true} style={style.frontEnd} onClick={this.handleClick} />
+                <br />
+                <RaisedButton label="Back End" value="Back End" primary={true} style={style.backEnd} onClick={this.handleClick} />
+            </div>
+            <div id="categoryList">
+                <CategoryList category={category} results={results} />
+            </div>
+            </div>
         )
     }
 }
